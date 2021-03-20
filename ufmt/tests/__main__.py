4f5a5f4eb3e00b081e931a18c4ddb72d918b8927
John Reese <john@noswap.com>
2021-03-20 15:55:26 -0700

    Basic testing/coverage

---
+++ ufmt/tests/__main__.py
@@ -0,0 +1,7 @@
+# Copyright 2021 John Reese
+# Licensed under the MIT license
+
+import unittest
+
+if __name__ == "__main__":  # pragma: no cover
+    unittest.main(module="ufmt.tests", verbosity=2)
---
+++ ufmt/tests/smoke.py
@@ -0,0 +1,9 @@
+# Copyright 2021 John Reese
+# Licensed under the MIT license
+
+from unittest import TestCase
+
+import ufmt
+
+class SmokeTest(TestCase):
+    pass
