398c934e5187c073ea69892edf972834b19360b2
John Reese <john@noswap.com>
2021-03-21 10:57:31 -0700

    Clean up __init__.py

--- ufmt/__init__.py
+++ ufmt/__init__.py
@@ -1,17 +1,7 @@
 """
-usort + black, in that order
+Safe, atomic formatting with black and µsort
 """
 
 __author__ = "John Reese"
-import sys
-from pathlib import Path
-from typing import List
-
-import click
-from black import decode_bytes, format_file_contents, Mode, NothingChanged
-from usort.config import Config
-from usort.sorting import usort_string
-from usort.util import walk
-
 from .__version__ import __version__
 from .core import ufmt_paths, ufmt_file, ufmt_string
