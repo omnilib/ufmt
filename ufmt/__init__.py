ba0ae8365678e39dc16e9d2c8b37089515e2df36
John Reese <john@noswap.com>
2022-05-21 18:13:50 -0700

    Export core API

--- ufmt/__init__.py
+++ ufmt/__init__.py
@@ -4,4 +4,11 @@ Safe, atomic formatting with black and µsort
 
 __author__ = "John Reese"
 from .__version__ import __version__
-from .core import ufmt_file, ufmt_paths, ufmt_string
+from .core import Result, ufmt_file, ufmt_paths, ufmt_string
+
+__all__ = [
+    "Result",
+    "ufmt_file",
+    "ufmt_paths",
+    "ufmt_string",
+]
