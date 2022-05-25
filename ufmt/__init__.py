93da9951923b1a05209f0165315b61dd3828ef52
John Reese <john@noswap.com>
2022-05-24 21:40:21 -0700

    Export ufmt_stdin

--- ufmt/__init__.py
+++ ufmt/__init__.py
@@ -4,7 +4,7 @@ Safe, atomic formatting with black and µsort
 
 __author__ = "John Reese"
 from .__version__ import __version__
-from .core import ufmt_bytes, ufmt_file, ufmt_paths, ufmt_string
+from .core import ufmt_bytes, ufmt_file, ufmt_paths, ufmt_stdin, ufmt_string
 from .types import (
     BlackConfig,
     BlackConfigFactory,
@@ -28,6 +28,7 @@ __all__ = [
     "ufmt_bytes",
     "ufmt_file",
     "ufmt_paths",
+    "ufmt_stdin",
     "ufmt_string",
     "UsortConfig",
     "UsortConfigFactory",
