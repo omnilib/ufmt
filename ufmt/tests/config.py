1c5ba5a4608150f4a173be1876b574ecbc6f6989
Amethyst Reese <amy@n7.gg>
2023-03-26 21:11:00 -0700

    Move temp directory handling into test case setup

--- ufmt/tests/config.py
+++ ufmt/tests/config.py
@@ -14,6 +14,13 @@ from ufmt.config import ufmt_config, UfmtConfig
 class ConfigTest(TestCase):
     maxDiff = None
 
+    def setUp(self):
+        self._td = TemporaryDirectory()
+        self.addCleanup(self._td.cleanup)
+
+        self.td = Path(self._td.name).resolve()
+        self.pyproject = self.td / "pyproject.toml"
+
     def test_ufmt_config(self):
         fake_config = dedent(
             """
@@ -22,61 +29,67 @@ class ConfigTest(TestCase):
                 "a",
                 "b",
             ]
-        """
+            """
         )
 
-        with TemporaryDirectory() as td:
-            td = Path(td).resolve()
-            foo = td / "foo"
-            foo.mkdir()
-            bar = foo / "bar.py"
-            bar.write_text("print('hello world')\n")
+        foo = self.td / "foo"
+        foo.mkdir()
+        bar = foo / "bar.py"
+        bar.write_text("print('hello world')\n")
 
-            with self.subTest("absolute path, no pyproject.toml"):
-                config = ufmt_config(bar)
+        with self.subTest("absolute path, no pyproject.toml"):
+            config = ufmt_config(bar)
+            self.assertEqual(UfmtConfig(), config)
+
+        with self.subTest("local path, no pyproject.toml"):
+            with cd(foo):
+                config = ufmt_config()
                 self.assertEqual(UfmtConfig(), config)
 
-            with self.subTest("local path, no pyproject.toml"):
-                with cd(foo):
-                    config = ufmt_config()
-                    self.assertEqual(UfmtConfig(), config)
+        self.pyproject.write_text("")
 
-            pyproj = td / "pyproject.toml"
-            pyproj.write_text("")
+        with self.subTest("absolute path, empty pyproject.toml"):
+            config = ufmt_config(bar)
+            self.assertEqual(
+                UfmtConfig(
+                    project_root=self.td, pyproject_path=self.pyproject, excludes=[]
+                ),
+                config,
+            )
 
-            with self.subTest("absolute path, empty pyproject.toml"):
-                config = ufmt_config(bar)
-                self.assertEqual(
-                    UfmtConfig(project_root=td, pyproject_path=pyproj, excludes=[]),
-                    config,
-                )
-
-            with self.subTest("local path, empty pyproject.toml"):
-                with cd(td):
-                    config = ufmt_config()
-                    self.assertEqual(
-                        UfmtConfig(project_root=td, pyproject_path=pyproj, excludes=[]),
-                        config,
-                    )
-
-            pyproj = td / "pyproject.toml"
-            pyproj.write_text(fake_config)
-
-            with self.subTest("absolute path, with pyproject.toml"):
-                config = ufmt_config(bar)
+        with self.subTest("local path, empty pyproject.toml"):
+            with cd(self.td):
+                config = ufmt_config()
                 self.assertEqual(
                     UfmtConfig(
-                        project_root=td, pyproject_path=pyproj, excludes=["a", "b"]
+                        project_root=self.td,
+                        pyproject_path=self.pyproject,
+                        excludes=[],
                     ),
                     config,
                 )
 
-            with self.subTest("local path, with pyproject.toml"):
-                with cd(td):
-                    config = ufmt_config()
-                    self.assertEqual(
-                        UfmtConfig(
-                            project_root=td, pyproject_path=pyproj, excludes=["a", "b"]
-                        ),
-                        config,
-                    )
+        self.pyproject.write_text(fake_config)
+
+        with self.subTest("absolute path, with pyproject.toml"):
+            config = ufmt_config(bar)
+            self.assertEqual(
+                UfmtConfig(
+                    project_root=self.td,
+                    pyproject_path=self.pyproject,
+                    excludes=["a", "b"],
+                ),
+                config,
+            )
+
+        with self.subTest("local path, with pyproject.toml"):
+            with cd(self.td):
+                config = ufmt_config()
+                self.assertEqual(
+                    UfmtConfig(
+                        project_root=self.td,
+                        pyproject_path=self.pyproject,
+                        excludes=["a", "b"],
+                    ),
+                    config,
+                )
