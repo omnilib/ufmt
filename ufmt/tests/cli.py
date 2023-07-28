034d2901b9c958ce69fa2bf742acf34787cec0e9
Sergey Goder <sergeygoder@gmail.com>
2023-07-27 22:13:17 -0700

    Return exit code 0 if there are no files (#151)
    See https://github.com/omnilib/ufmt/issues/148 for more context, this PR
    changes the behavior so if no files are found we print a message but the
    command returns successfully.

--- ufmt/tests/cli.py
+++ ufmt/tests/cli.py
@@ -107,7 +107,7 @@ class CliTest(TestCase):
             result = self.runner.invoke(main, ["check"])
             ufmt_mock.assert_called_with([Path(".")], dry_run=True, concurrency=None)
             self.assertRegex(result.stderr, r"No files found")
-            self.assertEqual(1, result.exit_code)
+            self.assertEqual(0, result.exit_code)
 
         with self.subTest("already formatted"):
             ufmt_mock.reset_mock()
@@ -178,7 +178,7 @@ class CliTest(TestCase):
                 [Path(".")], dry_run=True, diff=True, concurrency=None
             )
             self.assertRegex(result.stderr, r"No files found")
-            self.assertEqual(1, result.exit_code)
+            self.assertEqual(0, result.exit_code)
 
         with self.subTest("already formatted"):
             ufmt_mock.reset_mock()
@@ -268,7 +268,7 @@ class CliTest(TestCase):
             result = self.runner.invoke(main, ["format"])
             ufmt_mock.assert_called_with([Path(".")], concurrency=None)
             self.assertRegex(result.stderr, r"No files found")
-            self.assertEqual(1, result.exit_code)
+            self.assertEqual(0, result.exit_code)
 
         with self.subTest("already formatted"):
             ufmt_mock.reset_mock()
